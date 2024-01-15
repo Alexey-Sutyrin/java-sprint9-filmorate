dfb27d9a730ed9de06d55820a976e08a94ceb151
Alexey <vook-alf@yandex.ru>
2024-01-16 02:55:10 +0500

    Sprint 9 - Filmorate project - Commit 2 - Film-User-Controller

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,66 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exeptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.validator.FilmValidator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
+@Slf4j
+@RequestMapping("/films")
 public class FilmController {
+     
+    //Хранение списка добавленных фильмов
+
+    private final Map<Integer, Film> films = new HashMap<>();
+    private int idFilm = 0;
+
+
+    //получение списка всех фильмов
+
+    @GetMapping
+    public List<Film> getFilms() {
+        return new ArrayList<>(films.values());
+    }
+
+
+    //добавление фильма в список
+
+    @PostMapping()
+    public Film addFilm(@RequestBody Film film) throws ValidationException {
+        FilmValidator.isValidFilms(film);
+        int id = generateIdFilms();
+        film.setId(id);
+        log.debug("Сохранение: {}", film);
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    //обновление фильма в списке
+
+    @PutMapping()
+    public Film updateFilm(@RequestBody Film film) throws ValidationException {
+        if (films.containsKey(film.getId())) {
+            FilmValidator.isValidFilms(film);
+            log.debug("Обновление: {}", film);
+            films.put(film.getId(), film);
+        } else {
+            throw new ValidationException("Такого фильма нет в базе Filmorate.");
+        }
+        return film;
+    }
+
+     
+    //создание уникального id для фильма
+
+    private int generateIdFilms() {
+        return ++idFilm;
+    }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,11 +2,20 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
 
-/**
- * Film.
- */
+import java.time.LocalDate;
+
 @Getter
 @Setter
+@Data
+@Builder
 public class Film {
+    private int id;
+    @NonNull private String name;
+    @NonNull private String description;
+    @NonNull private LocalDate releaseDate;
+    @NonNull private int duration;
 }
