8bb4a3b007937463d3ba19f203155de9d698a58a
Alexey <vook-alf@yandex.ru>
2024-01-24 17:18:34 +0500

    Sprint 11 - Filmorate project - Second try - UserDBStorage Mapper fix

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/UserDbStorage.java
@@ -2,9 +2,7 @@ package ru.yandex.practicum.filmorate.storage.dao;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.filmorate.exception.UserDoesNotExistException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
