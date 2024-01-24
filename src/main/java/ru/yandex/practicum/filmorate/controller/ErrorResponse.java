3245109086d3c7a8feb32058fa465dc3a2364983
Alexey <vook-alf@yandex.ru>
2024-01-24 15:42:49 +0500

    Sprint 11 - Filmorate project - Second try - ErrorHandler logging update

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorResponse.java
@@ -5,10 +5,12 @@ public class ErrorResponse {
     private final String error;
 
     public ErrorResponse(String error) {
+
         this.error = error;
     }
 
     public String getError() {
+
         return error;
     }
 }
